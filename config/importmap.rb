48773f201c960bf735a309361d7098fae8638e97
Danny Iachini <danny.iachini@gmail.com>
2022-09-30 09:08:27 -0400

    Pin JS packages at 7.1.0 to show it working before
    ./bin/importmap pin @hotwired/turbo-rails@7.1.0
    ./bin/importmap pin @hotwired/turbo@7.1.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.0/app/javascript/turbo/index.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.1.0/dist/turbo.es2017-esm.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@6.1.7/src/index.js"
